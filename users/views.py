571041cdc9c772a75b25956036cc2ad61647279b
Ekanath ER <ekanather123@gmail.com>
2026-01-09 11:45:16 +0530

    Initial commit - Django blog project

---
+++ users/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect
+from .forms import SignUpForm,UserUpdateForm,ProfileUpdateForms
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
+
+# Create your views here.
+def sign_up(request):
+    if  request.method=="POST":
+
+      form=SignUpForm(request.POST)
+      if form.is_valid():
+          form.save()
+          return redirect('users-login')
+    else:
+        form=SignUpForm()
+    context={
+        'form':form,
+    }
+    return render(request,'users/sign_up.html',context)
+
+def logout_view(request):
+    logout(request)
+    return render(request,'users/logout.html')
+
+@login_required
+def profile(request):
+    if request.method=='POST':
+        u_form=UserUpdateForm(request.POST  or None,instance=request.user)
+        p_form=ProfileUpdateForms(request.POST  or None,request.FILES or None,instance=request.user.profilemodel)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('users-profile')
+    else:
+        u_form=UserUpdateForm(instance=request.user)
+        p_form=ProfileUpdateForms(instance=request.user.profilemodel)
+    context={
+        'u_form':u_form,
+        'p_form':p_form
+
+    }
+
+    return render(request,'users/profile.html',context)
